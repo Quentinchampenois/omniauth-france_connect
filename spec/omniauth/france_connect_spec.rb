4665875c94d45a71dac163889e0eedd21b2ba41d
moustachu <moustachu@opensourcepolitics.eu>
2022-08-02 13:07:51 +0200

    forget to bump version number to 0.1.0

--- spec/omniauth/france_connect_spec.rb
+++ spec/omniauth/france_connect_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe OmniAuth::FranceConnect do
   it "has a version number" do
-    expect(OmniAuth::FranceConnect::VERSION).to eq("0.0.9")
+    expect(OmniAuth::FranceConnect::VERSION).to eq("0.1.0")
   end
 end
